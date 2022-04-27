102c3d4b889d9a56006d9675343c3651e02af3b2
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-04-27 22:56:51 +0200

    syntax update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import EventList from './EventList';
 
 function App() {
   return (
     <div className="App">
-
+      <EventList />
     </div>
   );
 }
