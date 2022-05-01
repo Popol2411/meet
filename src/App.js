6b55d82edb3b0257b52c7920847ddbd6d4d0e3e9
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-05-01 21:19:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     currentLocation: 'all'
   }
 
-  updateEvents = (location) => {
+  updateEvents = (location, numberOfEvents) => {
     getEvents().then((events) => {
       const locationEvents = (location === 'all') ?
         events :
