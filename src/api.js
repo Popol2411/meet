32c113df5039277ebe838af7e8613de113ddf8e7
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-05-02 11:25:34 +0200

    updated syntax

--- src/api.js
+++ src/api.js
@@ -13,13 +13,21 @@ import axios from 'axios';
 
 import NProgress from 'nprogress';
 
-export const extractLocations = (events) => {
-  var extractLocations = events.map((event) => event.location);
-  var locations = [...new Set(extractLocations)];
-  return locations;
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch('https://cvsgxj18e0.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode)
+
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
+
+  access_token && localStorage.setItem('access_token', access_token);
+
+  return access_token;
 };
 
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
@@ -55,16 +63,22 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'https://cvsgxj18e0.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/token';
+    const url = 'https://cvsgxj18e0.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
-      localStorage.setItem("lastEvents", JSON.stringify(result.data));
-      localStorage.setItem("locations", JSON.stringify(locations));
+      localStorage.setItem('lastEvents', JSON.stringify(result.data));
+      localStorage.setItem('locations', JSON.stringify(locations));
     }
     NProgress.done();
     return result.data.events;
   }
+};
+
+export const extractLocations = (events) => {
+  var extractLocations = events.map((event) => event.location);
+  var locations = [...new Set(extractLocations)];
+  return locations;
 };
 
 export const getAccessToken = async () => {
@@ -72,31 +86,16 @@ export const getAccessToken = async () => {
   const tokenCheck = accessToken && (await checkToken(accessToken));
 
   if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
+    await localStorage.removeItem('access_token');
     const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
+    const code = await searchParams.get('code');
     if (!code) {
       const results = await axios.get(
-        "https://cvsgxj18e0.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
-      );
+        'https://cvsgxj18e0.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url');
       const { authUrl } = results.data;
       return (window.location.href = authUrl);
     }
     return code && getToken(code);
   }
   return accessToken;
-}
-
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const { access_token } = await fetch('https://cvsgxj18e0.execute-api.eu-central-1.amazonaws.com/dev/api/token/encodeCode')
-
-    .then((res) => {
-      return res.json();
-    })
-    .catch((error) => error);
-
-  access_token && localStorage.setItem("access_token", access_token);
-
-  return access_token;
-}; 
+};
