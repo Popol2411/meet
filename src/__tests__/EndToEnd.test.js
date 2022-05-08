3a08ecb03fbbe79bd2c13c73a34d1b3da1f319da
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-05-08 22:20:43 +0200

    Update to EndToEnd Test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -3,9 +3,13 @@ import puppeteer from 'puppeteer';
 describe('show/hide an event details', () => {
   let browser;
   let page;
+  jest.setTimeout(30000);
   beforeAll(async () => {
-    jest.setTimeout(30000);
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250, // slow down by 250ms
+      ignoreDefaultArgs: ['--disable-extensions'], // ignores default setting that causes timeout errors
+    });
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
@@ -24,7 +28,7 @@ describe('show/hide an event details', () => {
 
   test('User can expand an event to see its details', async () => {
 
-    await page.click('.event .details-button');
+    await page.click('.event .show-details');
     const eventDetails = await page.$('.event .extra-details');
     expect(eventDetails).toBeDefined();
 
