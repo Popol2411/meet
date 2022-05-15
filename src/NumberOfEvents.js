08220dd5691e9f077d6d5967dd38bfa2d794fe50
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-05-15 12:26:48 +0200

    infoText to be displayed when event number is missing added

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
+
+  state = {
+    numberOfEvents: '32',
+  };
+
+  handleInputChanged = (event) => {
+    const number = event.target.value;
+    if (number < 1 || number > 32) {
+      this.setState({
+        numberOfEvents: number,
+        infoText: 'Enter a number between 1 and 32',
+      });
+    } else {
+      this.setState({
+        numberOfEvents: number,
+        infoText: '',
+      });
+    }
+    this.props.updateNumberOfEvents(event.target.value);
+  };
 
 
   render() {
@@ -8,11 +29,14 @@ class NumberOfEvents extends Component {
       <div className="NumberOfEvents">
         <h4>Number of events to display</h4>
         <input
+        min={1}
+        max={32}
           className="number-of-events"
           type="number"
-          value={this.props.numberOfEvents}
-          onChange={(e) => this.props.updateNumberOfEvents(e)}>
-        </input>
+          value={this.state.numberOfEvents}
+          onChange={this.handleInputChanged}
+        />
+        <ErrorAlert text={this.state.infoText} />
       </div>
     );
   }
