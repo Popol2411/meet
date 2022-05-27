6cfccf990e6d2e419ea1c872a9cdafa27aefe75a
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-05-27 22:33:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NumberOfEvents from './NumberOfEvents';
 import WelcomeScreen from './WelcomeScreen';
 import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import { OfflineAlert } from './Alert';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
   state = {
@@ -119,7 +119,8 @@ class App extends Component {
         </div>
         <OfflineAlert id='OfflineAlert' text={offlineText} />
 
-        <ScatterChart width={730} height={250}
+        <ResponsiveContainer height={400} >
+        <ScatterChart 
         margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
         <CartesianGrid strokeDasharray="3 3" />
             <XAxis type="category" dataKey="city" name="city" />
@@ -127,6 +128,7 @@ class App extends Component {
             <Tooltip cursor={{ strokeDasharray: '3 3' }} />
             <Scatter data={this.getData()} fill="#8884d8" />
         </ScatterChart>
+        </ResponsiveContainer>
 
         <EventList events={events} />
         <OfflineAlert text={this.state.OfflineAlertText} />
