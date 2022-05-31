ef7236058413c28810ede3e4350c957bea5bdf53
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-05-31 09:58:44 +0200

    syntax correction

--- src/api.js
+++ src/api.js
@@ -76,7 +76,7 @@ export const getEvents = async () => {
       token;
     const result = await axios.get(url);
     if (result.data) {
-      var locations = extractLocations(result.data.events);
+      const locations = extractLocations(result.data.events);
       localStorage.setItem('lastEvents', JSON.stringify(result.data));
       localStorage.setItem('locations', JSON.stringify(locations));
     }
