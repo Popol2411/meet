99fb249e12fcbc0419b2c9aa5e72f37ebd969e3a
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-06-02 17:16:28 +0200

    Update Alert.js

--- src/Alert.js
+++ src/Alert.js
@@ -15,7 +15,7 @@ class Alert extends Component {
   render() {
     return (
       <div className="Alert">
-        <p style={this.getStyle()}>{this.props.text}</p>
+        <Alert style={this.getStyle()}>{this.props.text}</Alert>
       </div>
     );
   }
